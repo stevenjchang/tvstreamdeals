590f6eac9a6bf3336f0394f5e1ffc47841518ee6
stevenjchang <stevenjchang@gmail.com>
2021-04-24 16:17:35 -0700

    style: add page layout

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -8,9 +8,43 @@ import Aws from '@/constants/svg/aws.svg';
 const headerStyle = css`
   background-color: #ffffff;
   min-height: calc(100vh - 6rem);
+  text-align: center;
 `;
 
+const Team = ({ children }: any) => (
+  <h1 className={tw(`font-sans font-bold text-4xl md:text-5xl lg:text-8xl text-center leading-snug text-gray-800`)}>
+    {children}
+  </h1>
+);
+
 const Header = () => (
+  <header className={tw(headerStyle)}>
+    <div className={tw(`max-w-4xl mx-auto py-16 px-14 sm:px-6 lg:px-8 flex flex-col`)}>
+      <Team>Celtics</Team>
+      <p className={tw(`text-xl`)}>at</p>
+      <Team>Nets</Team>
+      <div className={tw(`max-w-xl mx-auto`)}>
+        <p className={tw(`mt-10 text-gray-500 text-xl lg:text-3xl`)}>4:05 PM Today</p>
+      </div>
+      <div className={tw(`mt-10 flex justify-center items-center w-full mx-auto`)}>
+        <Button large>Stream Game Live </Button>
+      </div>
+    </div>
+    <div className={tw(`flex justify-center w-full`)}>
+      <div className={tw(`mt-4 w-full`)}>
+        <p className={tw(`font-mono uppercase text-center font-medium text-sm text-gray-600`)}>These folks get it</p>
+        <div className={tw(`flex items-center justify-center mx-auto flex-wrap`)}>
+          <Aws className={tw(`m-12 mb-8`)} width={120} />
+          <Netlify className={tw(`m-12`)} width={140} />
+          <Nike className={tw(`m-12`)} width={140} />
+          <Figma className={tw(`m-12`)} width={140} />
+        </div>
+      </div>
+    </div>
+  </header>
+);
+
+const Header_old = () => (
   <header className={tw(headerStyle)}>
     <div className={tw(`max-w-4xl mx-auto py-16 px-14 sm:px-6 lg:px-8`)}>
       <h1 className={tw(`font-sans font-bold text-4xl md:text-5xl lg:text-8xl text-center leading-snug text-gray-800`)}>
