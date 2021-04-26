62a04f61f6c3ba47f5b6007d00394a0e48923246
stevenjchang <stevenjchang@gmail.com>
2021-04-25 18:29:13 -0700

    feat(scripts): convert raw data

---
+++ scripts/excelToJson.js
@@ -0,0 +1,47 @@
+/* eslint-disable */
+
+const fs = require('fs');
+
+const transformData = (data) => {
+  return data.map((row) => {
+    return {
+      team1: row.team1,
+      team2: row.team2,
+      tv: row.TV,
+      date: row.Date,
+      time: row['ET Standard'],
+    };
+  });
+};
+
+const writeFile = (data) => {
+  fs.writeFile('./src/data/games.json', data, (err) => console.log('writeFile Error ==>', err));
+};
+
+const rawToFormattedGameData = () => {
+  const rawData = fs.readFileSync('./src/data/raw/rawGames.json');
+  const jsonData = JSON.parse(rawData);
+  const newData = transformData(jsonData.rawData);
+  writeFile(JSON.stringify(newData, null, 4));
+  console.log('excelToJson successful ==>');
+};
+
+const formatTeamMapping = () => {
+  const rawData = fs.readFileSync('./src/data/raw/rawTeam.json');
+  // console.log('nbaTeamMapping ==>', nbaTeamMapping);
+  const jsonData = JSON.parse(rawData).data;
+  const newData = jsonData.map((item) => {
+    return {
+      ...item,
+      teamId: null,
+      abbreviation: item.abbreviation.toLowerCase(),
+    };
+  });
+  fs.writeFile('./src/utils/gameUtils/teamMapping.json', JSON.stringify(newData, null, 4), (err) =>
+    console.log('writeFile error ==>', err),
+  );
+  console.log('formatTeamMapping successful ==>', formatTeamMapping);
+};
+
+// rawToFormattedGameData();
+formatTeamMapping();
--- src/utils/GameUtils.ts
+++ src/utils/GameUtils.ts
@@ -9,14 +9,30 @@ const teamMapping: any = {
   },
 };
 
+function toDisplayTime(timeAsString) {
+  const h = timeAsString.substring(0, 2);
+  const hour = h[0] === '0' ? h[1] : h;
+  const minute = timeAsString.substring(2, 4);
+  return `${hour}:${minute} PM`;
+}
+
+function toDisplayDate(str) {
+  const d = new Date(str);
+  console.log('d ==>', d);
+}
 class GameUtils {
   static parseGameParamKey = (key: string | any) => {
     const date = key.substring(0, 6);
-    const t1 = key.substring(6, 9);
-    const t2 = key.substring(9, 12);
+    const time = key.substring(6, 10);
+    const t1 = key.substring(10, 13);
+    const t2 = key.substring(13, 16);
     const team1 = GameUtils.getTeam(t1);
     const team2 = GameUtils.getTeam(t2);
-    return { date, team1, team2 };
+
+    const test = toDisplayDate(date);
+    console.log('test ==>', test);
+
+    return { date, team1, team2, time: toDisplayTime(time) };
   };
 
   static getTeam(abbr: string): Record<string, any> {
@@ -25,7 +41,6 @@ class GameUtils {
 
   static toLocalTime() {
     const offset = new Date().getTimezoneOffset() / 60;
-    console.log('offset ==>', offset);
   }
 }
 
